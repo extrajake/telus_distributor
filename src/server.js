04d28a7271bcea0d86ed9d4d02fc82287c05051c
extrajake <extrajelly@live.ca>
2023-06-24 00:19:49 +1000

    changes functions locations in netlify.toml

--- src/server.js
+++ src/server.js
@@ -37,9 +37,9 @@ app.post("/", function(req, res){
     }
 });
 
-app.listen(3000, function(){
-    console.log('server is running on port 3000.');
-})
+// app.listen(3000, function(){
+//     console.log('server is running on port 3000.');
+// })
 
 // app.use("/.netlify/functions/server", router);
 
