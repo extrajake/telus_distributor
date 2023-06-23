90cfbc200d1d5b030e49c04d295aedced2473a68
extrajake <extrajelly@live.ca>
2023-06-23 16:36:05 +1000

    try again

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const serverless = require('serverless-http')
-const bodyParser = require('body-parser');
+
 
 const app = express();
+const bodyParser = require('body-parser');
+
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(express.static(__dirname + '/public'));
