a6880623a685dcb676ce84640e45c2ab737d6b90
extrajake <extrajelly@live.ca>
2023-06-24 15:09:38 +1000

    for some reason i had an a tag directing to new page on submit button

--- functions/server.js
+++ functions/server.js
@@ -6,17 +6,17 @@ const https = require('https');
 const app = express();
 const bodyParser = require('body-parser');
 
-// const router = express.Router()
+const router = express.Router()
 
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(express.static(__dirname + '/public'));
 
-app.get("/", function(req, res) {
+router.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/", function(req, res){
+router.post("/", function(req, res){
     const codes = ["1NKCI", "V3L7R", "60HPC", "8SF6L", "AHG08"]
     const codes2 = ["2WEMV", "KWWNI", "XNB7X", "6BN2T", "Y2FOK"]
     const codes3 = ["YJYZL", "6HNP2", "V9FF8", "8SF5L", "5B23I"]
@@ -24,15 +24,15 @@ app.post("/", function(req, res){
     const codes5 = ["KGFIA", "XI28K", "2QQ8K", "9Y4HK", "DMQJT"]
 
     if (codes.includes(req.body.field)) {
-        res.sendFile(__dirname + "./promo1.html");
+        res.sendFile(__dirname + "/promo1.html");
     } else if (codes2.includes(req.body.field)) {
-        res.sendFile(__dirname + "./promo2.html");
+        res.sendFile(__dirname + "/promo2.html");
     } else if (codes3.includes(req.body.field)) {
-        res.sendFile(__dirname + "./promo3.html");
+        res.sendFile(__dirname + "/promo3.html");
     } else if (codes4.includes(req.body.field)) {
-        res.sendFile(__dirname + "./promo4.html");
+        res.sendFile(__dirname + "/promo4.html");
     } else if (codes5.includes(req.body.field)) {
-        res.sendFile(__dirname + "./promo5.html");
+        res.sendFile(__dirname + "/promo5.html");
     } else {
         res.send("The code is invalid");
     }
@@ -42,6 +42,6 @@ app.listen(3000, function(){
     console.log('server is running on port 3000.');
 })
 
-// app.use("/.netlify/functions/server", router);
+app.use("/.netlify/functions/server", router);
 
 module.exports.handler = serverless(app);
