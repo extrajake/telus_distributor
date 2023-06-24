2e5cd1a73a1669a1f82ea4e02a20be34b918d9c9
extrajake <extrajelly@live.ca>
2023-06-24 16:33:01 +1000

    added index to post route

--- functions/server.js
+++ functions/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(express.static(__dirname + '/public'));
 
-app.get("/", function(req, res) {
+app.get("/index", function(req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
