dbc4e15e94e5d616b620804c4e408081ff951778
extrajake <extrajelly@live.ca>
2023-06-25 15:04:46 +1000

    testing app.pot

--- functions/app.js
+++ functions/app.js
@@ -14,25 +14,27 @@ app.get('/', function (req, res) {
 });
 
 app.post('/index.html', function (req, res) {
-  const codes = ["1NKCI", "V3L7R", "60HPC", "8SF6L", "AHG08"];
-  const codes2 = ["2WEMV", "KWWNI", "XNB7X", "6BN2T", "Y2FOK"];
-  const codes3 = ["YJYZL", "6HNP2", "V9FF8", "8SF5L", "5B23I"];
-  const codes4 = ["UVUNU", "1N5UL", "IINVK", "AEZNP", "HM2NM"];
-  const codes5 = ["KGFIA", "XI28K", "2QQ8K", "9Y4HK", "DMQJT"];
 
-  if (codes.includes(req.body.field)) {
-    res.redirect('/public/promo1.html');
-  } else if (codes2.includes(req.body.field)) {
-    res.redirect('/public/promo2.html');
-  } else if (codes3.includes(req.body.field)) {
-    res.redirect('/public/promo3.html');
-  } else if (codes4.includes(req.body.field)) {
-    res.redirect('/public/promo4.html');
-  } else if (codes5.includes(req.body.field)) {
-    res.redirect('/public/promo5.html');
-  } else {
-    res.send('The code is invalid');
-  }
+  res.send('success');
+  // const codes = ["1NKCI", "V3L7R", "60HPC", "8SF6L", "AHG08"];
+  // const codes2 = ["2WEMV", "KWWNI", "XNB7X", "6BN2T", "Y2FOK"];
+  // const codes3 = ["YJYZL", "6HNP2", "V9FF8", "8SF5L", "5B23I"];
+  // const codes4 = ["UVUNU", "1N5UL", "IINVK", "AEZNP", "HM2NM"];
+  // const codes5 = ["KGFIA", "XI28K", "2QQ8K", "9Y4HK", "DMQJT"];
+
+  // if (codes.includes(req.body.field)) {
+  //   res.redirect('/promo1.html');
+  // } else if (codes2.includes(req.body.field)) {
+  //   res.redirect('/promo2.html');
+  // } else if (codes3.includes(req.body.field)) {
+  //   res.redirect('/promo3.html');
+  // } else if (codes4.includes(req.body.field)) {
+  //   res.redirect('/promo4.html');
+  // } else if (codes5.includes(req.body.field)) {
+  //   res.redirect('/promo5.html');
+  // } else {
+  //   res.send('The code is invalid');
+  // }
 });
 
 module.exports.handler = serverless(app);
